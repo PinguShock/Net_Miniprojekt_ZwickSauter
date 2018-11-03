86ace7eddd34ba54efccd7c0196af7fdee48e19d
Martin <msauter@hsr.ch>
2018-11-03 15:57:32 +0100

    Projektdateien hinzufügen.

---
+++ AutoReservation.BusinessLayer/AutoManager.cs
@@ -0,0 +1,21 @@
+﻿namespace AutoReservation.BusinessLayer
+{
+    public class AutoManager
+        : ManagerBase
+    {
+        // Example
+        //public List<Auto> List
+        //{
+        //    get
+        //    {
+        //        using (AutoReservationContext context = new AutoReservationContext())
+        //        {
+        //            return context.Autos.ToList();
+        //        }
+        //    }
+        //}
+
+        // Hoi David
+
+    }
+}
