7946e41eea8656c7f0193a7c8daf7551c9613c5a
martin <msauter@hsr.ch>
2018-11-25 15:36:52 +0100

    Struktur teilweise Übernommen

---
+++ AutoReservation.GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoReservation.Common.Extensions;
+
+namespace AutoReservation.GUI.ViewModels
+{
+    class ViewModelBase : IExtendedNotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void IExtendedNotifyPropertyChanged.OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged == null)
+            {
+                return;
+            }
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
